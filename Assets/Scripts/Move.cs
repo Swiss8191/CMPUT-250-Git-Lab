b639124127fff712bfed95bbc5ac8ba699e3033a
Swiss8191 <swisscheese819@gmail.com>
2026-09-14 19:30:07 -0600

    Added spinning chicken and changed UI text

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private float speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
